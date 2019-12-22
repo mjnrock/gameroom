1907ba6c9a7045a59f7f3286531674dbdcefd7c0
Matt Nawrocki <mjnrock@umich.edu>
2019-12-21 21:27:29 -0500

    init

--- server/index.js
+++ server/index.js
@@ -1,9 +1,34 @@
 const express = require("express");
-const app = express();
-const expressWs = require("express-ws")(app);
+const server = express();
+const expressWs = require("express-ws")(server);
+const aWss = expressWs.getWss('/');
 const path = require("path");
 
 const PORT = 3000;
+const App = {
+    Loop: null,
+    Clients: {},
+    Score: {
+        1: 0,
+        2: 0
+    },
+    Ball: {
+        X: 0,
+        Y: 0,
+        Hx: true,
+        Hy: true
+    },
+    Players: {
+        1: {
+            X: 0,
+            Y: 0
+        },
+        2: {
+            X: 0,
+            Y: 0
+        }
+    }
+};
 require("dns").lookup(require("os").hostname(), function(err, ip, fam) {
     console.log(`Started Gameroom server at ${ ip }:${ PORT }`);
 });
@@ -15,37 +40,208 @@ function GenerateUUID() {
     });
 }
 
-app.set("trust proxy", true);
+server.set("trust proxy", true);
 // app.use(function (req, res, next) {
 //     console.log("middleware");
 //     req.testing = "testing";
 //     return next();
 // });
 
-app.get("/", (req, res) => {
-    res.sendFile(`${ __dirname }/index.html`);
+server.get("/", (req, res) => {
+    res.sendFile(`${ __dirname }/client/controller.html`);
+});
+server.get("/v", (req, res) => {
+    res.sendFile(`${ __dirname }/client/viewport.html`);
 });
 
-const clients = {};
 expressWs.getWss().on("connection", ws => {
     let uuid = GenerateUUID();
 
     ws.UUID = GenerateUUID();
-    clients[ uuid ] = ws;
+    App.Clients[ uuid ] = ws;
+
+    ws.send(JSON.stringify({
+        event: "uuid",
+        uuid
+    }));
+    SendViewport();
 
     console.log(`Added: ${ uuid }`);
-    console.log(`Size: ${ Object.keys(clients).length }`);
-    console.log(`Clients: ${ Object.keys(clients) }`);
+    console.log(`Size: ${ Object.keys(App.Clients).length }`);
+    console.log(`Clients: ${ Object.keys(App.Clients) }`);
 });
 
-app.ws("/", function (ws, req) {
+server.ws("/", function (ws, req) {
     ws.on("message", function (msg) {
-        console.log(msg);
+        try {
+            console.log(msg);
+
+            let data = JSON.parse(msg);
+
+            if(data.event === "viewport") {
+                SendViewport();
+            } else if(data.event === "touch") {
+                App.Players[ data.player ].Y = +data.y;
+                    
+                if(App.Players[ data.player ].Y < 0) {                        
+                    App.Players[ data.player ].Y = 0;
+                }
+                if(App.Players[ data.player ].Y > 500) {
+                    App.Players[ data.player ].Y = 500;
+                }
+
+                if(data.player == 1) {
+                    App.Players[ data.player ].X = 100;
+                }
+                if(data.player == 2) {
+                    App.Players[ data.player ].X = 400;
+                }
+
+                SendViewport();
+            } else if(data.event === "move") {
+                let { direction, magnitude } = data;
+
+                if(direction === "x") {
+                    // if(magnitude === "+") {
+                    //     App.Players[ data.player ].X += 5;
+                    // } else if(magnitude === "-") {
+                    //     App.Players[ data.player ].X -= 5;
+                    // }
+                } else if(direction === "y") {
+                    if(magnitude === "+") {
+                        App.Players[ data.player ].Y += 5;
+                    } else if(magnitude === "-") {
+                        App.Players[ data.player ].Y -= 5;
+                    }
+                    
+                    if(App.Players[ data.player ].Y < 0) {                        
+                        App.Players[ data.player ].Y = 0;
+                    }
+                    if(App.Players[ data.player ].Y > 500) {
+                        App.Players[ data.player ].Y = 500;
+                    }
+
+                    if(data.player == 1) {
+                        App.Players[ data.player ].X = 100;
+                    }
+                    if(data.player == 2) {
+                        App.Players[ data.player ].X = 400;
+                    }
+                }
+
+                SendViewport();
+            }
+        } catch(e) {
+            console.log(`[Message Failed]: `, JSON.stringify(msg));
+        }
     });
 });
 
-setTimeout(() => {
-    Object.values(clients).forEach(client => client.send(client.UUID));
-}, 5000);
+function SendViewport() {
+    SendAllMessage({
+        event: "viewport",
+        clients: Object.keys(App.Clients),
+        players: App.Players,
+        ball: App.Ball
+    });
+}
 
-const SERVER = app.listen(PORT);
+function SendScore() {
+    SendAllMessage({
+        event: "score",
+        score: App.Score
+    });
+}
+function SendAllMessage(obj) {
+    aWss.clients.forEach(function (client) {
+        client.send(JSON.stringify(obj));
+    });
+}
+
+const SERVER = server.listen(PORT);
+
+function ResetBall() {
+    App.Ball.X = 250;
+    App.Ball.Y = 250;
+    App.Ball.Hx = true;
+    App.Ball.Hy = true;
+}
+function isCollision(a, b) {
+    return !(
+        ((a.Y + a.H) < (b.Y)) ||
+        (a.Y > (b.Y + b.H)) ||
+        ((a.X + a.W) < b.X) ||
+        (a.X > (b.X + b.W))
+    );
+}
+App.Loop = setInterval(() => {
+    let pw = 25,
+        ph = 100,
+        bw = 10,
+        step = 5;
+
+    let bx = App.Ball.X,
+        by = App.Ball.Y,
+        x1 = App.Players[ "1" ].X,
+        y1 = App.Players[ "1" ].Y,
+        x2 = App.Players[ "2" ].X,
+        y2 = App.Players[ "2" ].Y;
+
+    if(isCollision({
+        X: bx,
+        Y: by,
+        H: bw,
+        W: bw
+    }, {
+        X: x1,
+        Y: y1,
+        H: ph,
+        W: pw
+    })) {
+        App.Ball.Hx = !App.Ball.Hx;
+    }
+    if(isCollision({
+        X: bx + bw,
+        Y: by,
+        H: bw,
+        W: bw
+    }, {
+        X: x2,
+        Y: y2,
+        H: ph,
+        W: pw
+    })) {
+        App.Ball.Hx = !App.Ball.Hx;
+    }
+
+    if(bx - bw <= 0) {
+        App.Ball.Hx = !App.Ball.Hx;
+    }
+    if(bx + bw >= 500) {
+        App.Ball.Hx = !App.Ball.Hx;
+    }
+    if(by - bw <= 0) {
+        App.Ball.Hy = !App.Ball.Hy;
+    }
+    if(by + bw >= 500) {
+        App.Ball.Hy = !App.Ball.Hy;
+    }
+
+    App.Ball.X += (App.Ball.Hx ? 1 : -1) * step;
+    App.Ball.Y += (App.Ball.Hy ? 1 : -1) * step;
+
+    if(App.Players[ "1" ].X > 0 && App.Players[ "2" ].X > 0) {
+        if(App.Ball.X < App.Players[ "1" ].X + pw) {
+            App.Score[ "2" ] += 1;
+            SendScore();
+            ResetBall();
+        }
+        if(App.Ball.X > App.Players[ "2" ].X) {
+            App.Score[ "1" ] += 1;
+            SendScore();
+            ResetBall();
+        }
+    }
+
+    SendViewport();
+}, 1000 / 25);
