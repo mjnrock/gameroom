af255c398624e9dae19aa74810c8e1556380b226
Matt Nawrocki <mjnrock@umich.edu>
2019-12-31 16:17:48 -0500

    init

--- gameroom-client/src/modules/controller/ControllerManager.js
+++ gameroom-client/src/modules/controller/ControllerManager.js
@@ -8,6 +8,20 @@ export default class ControllerManager extends AModuleManager {
         );
 
         this.prop("Controllers", {});
+        
+        //? Throttle event firing, mainly to be used for "user-events", but could be expanded if abstracted into throttled call
+        this.prop("Throttle", { // in milliseconds (e.g. 15 = 15ms)
+            "user-event": {
+                "keydown": 50,
+                "keyup": 50,
+            }
+        });
+        this.prop("Log", { // Last time that event was called
+            "user-event": {
+                "keydown": 0,
+                "keyup": 0,
+            }
+        });
 
         this.on("controller-event");
         this.on("user-event");
@@ -16,21 +30,31 @@ export default class ControllerManager extends AModuleManager {
 
         //! This is off for DEBUGGING reasons only, so as not to pollute `console.log`
         //? Once the Handler has been finalized, this won't have to result in that console logging
-        // this.AttemptHTMLBindings();
+        this.AttemptHTMLBindings();
     }
 
     AttemptHTMLBindings() {        
         if(window) {
-            window.onkeydown = (e) => this.call("user-event", [
-                e.type,
-                e.which,
-                e
-            ]);
-            window.onkeyup = (e) => this.call("user-event", [
-                e.type,
-                e.which,
-                e
-            ]);
+            let keyListener = (e) => {
+                let now = Date.now(),
+                    Throttle = this.prop("Throttle"),
+                    Log = this.prop("Log"),
+                    evt = "user-event";
+
+                if(now - Throttle[ evt ][ e.type ] >= Log[ evt ][ e.type ]) {
+                    this.call(evt, [
+                        e.type,
+                        e.which,
+                        e
+                    ]);
+                    
+                    Log[ evt ][ e.type ] = now;
+                    this.prop("Log", Log);
+                }
+            }
+
+            window.onkeydown = (e) => keyListener(e);
+            window.onkeyup = (e) => keyListener(e);
         }
     }
 
